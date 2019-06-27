e2f88734b1fe25769fbc1d307dc51d74dc7fea57
Klose Li <iliwgg@gmail.com>
2019-06-27 17:41:42 +0800

    add auth model

--- models/auth.go
+++ models/auth.go
@@ -1,7 +1,67 @@
 package models
 
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"io"
+)
+
+// Auth struct model represent auth table
 type Auth struct {
+	BaseModel
 	UserName string `json:"userName" gorm:"type:varchar(16)"`
 	Password string `json:"password" gorm:"type:varchar(255)"`
 	Email    string `json:"email" gorm:"type:varchar(128)"`
+}
+
+var ErrAuthExist = errors.New("auth already exists")
+
+// AddAuth func to add a new auth
+func AddAuth(username, password, email string) error {
+	// transaction
+	trx := db.Begin()
+	defer trx.Commit()
+
+	auth := Auth{}
+
+	trx.Set("gorm:query_option", "FOR UPDATE").
+		Where("user_name = ?", username).
+		First(&auth)
+
+	if auth.ID > 0 {
+		return ErrAuthExist
+	}
+
+	hash := md5.New()
+	io.WriteString(hash, password)
+	auth.UserName = username
+	auth.Password = fmt.Sprintf("%x", hash.Sum(nil))
+	auth.Email = email
+	err := trx.Create(&auth).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// CheckAuth func check if the auth is valid
+func CheckAuth(username, password string) bool {
+	trx := db.Begin()
+	defer trx.Commit()
+
+	auth := Auth{}
+
+	hash := md5.New()
+	io.WriteString(hash, password)
+	password = fmt.Sprintf("%x", hash.Sum(nil))
+
+	trx.Set("gorm:query_option", "FOR UPDATE").
+		Where("user_name = ? AND password = ?", username, password).
+		First(&auth)
+
+	if auth.ID > 0 {
+		return true
+	}
+	return false
 }
