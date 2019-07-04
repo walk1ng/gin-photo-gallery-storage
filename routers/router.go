9d002346566b9ad90fa97d2ed4c719293b304f0c
Klose Li <iliwgg@gmail.com>
2019-07-04 10:39:11 +0800

    update router for bucket api

--- routers/router.go
+++ routers/router.go
@@ -3,12 +3,17 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	v1 "github.com/walk1ng/gin-photo-gallery-storage/apis/v1"
+	"github.com/walk1ng/gin-photo-gallery-storage/middlewares"
 )
 
 var Router *gin.Engine
 
 func init() {
 	Router = gin.Default()
+
+	authMiddleware := middlewares.GetAuthMiddleware()
+	refreshMiddleware := middlewares.GetRefreshMiddleware()
+	paginationMiddleware := middlewares.GetPaginationMiddleware()
 
 	v1Group := Router.Group("/api/v1")
 	{
@@ -17,6 +22,16 @@ func init() {
 		{
 			authGroup.POST("/add", v1.AddAuth)
 			authGroup.POST("/check", v1.CheckAuth)
+		}
+
+		// bucket
+		bucketGroup := v1Group.Group("/bucket")
+		{
+			bucketGroup.POST("/add", authMiddleware, refreshMiddleware, v1.AddBucket)
+			bucketGroup.DELETE("/delete", authMiddleware, refreshMiddleware, v1.DeleteBucket)
+			bucketGroup.PUT("/update", authMiddleware, refreshMiddleware, v1.UpdateBucket)
+			bucketGroup.GET("/get_by_id", authMiddleware, refreshMiddleware, v1.GetBucketByID)
+			bucketGroup.GET("/get_by_auth_id", authMiddleware, refreshMiddleware, paginationMiddleware, v1.GetBucketByAuthID)
 		}
 	}
 }
